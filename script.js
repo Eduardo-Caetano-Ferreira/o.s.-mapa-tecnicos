8d3e0a1afd832995f1de538a9e1f090995bb3245
Eduardo Caetano Ferreira <101872104+Eduardo-Caetano-Ferreira@users.noreply.github.com>
2026-02-17 12:00:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const rotasMoto = [
     { nome: "Alexsandro", bairros: ["peruibe", "gaivota", "luiza mar", "jamaica", "jardim regina", "bopiranga", "santa julia"] },
     { nome: "Gabriel Batista", bairros: ["sao fernando", "tupy", "cibratel 2", "coronel", "umuarama", "sabauna", "guapiranga", "corumba", "belas artes", "praia dos sonhos"] },
     { nome: "Lucas Rodrigues", bairros: ["centro", "cidade anchieta", "satelite", "vila sao paulo", "laranjeiras", "oasis", "savoy", "guapura", "tropical", "nova itanhaem", "magalhaes", "sion", "suarao"] },
-    { nome: "Renan", bairros: ["campos eliseos", "verde mar", "santa terezinha", "loty", "florida mirim", "itaguai", "agenor de campos", "regina maria", "santana"] },
-    { nome: "Clayton", bairros: ["jardim leonor", "vila seabra", "nossa senhora de fatima", "vila atlantica", "umuarama", "vera cruz", "mongagua", "pedreira", "centro", "vila sao paulo", "vila nova"] },
+    { nome: "Renan", bairros: ["campos eliseos", "verde mar", "santa terezinha", "loty", "florida mirim", "itaguai", "agenor de campos", "regina maria", "santana", "jardim leonor", "jussara", "itaoca", "jardim praia grande"] },
+    { nome: "Clayton", bairros: ["vila seabra", "nossa senhora de fatima", "vila atlantica", "umuarama", "vera cruz", "mongagua", "pedreira", "centro", "vila sao paulo", "vila nova", "praia grande"] },
     { nome: "Augusto", bairros: ["itanhaem", "mongagua"] },
     { nome: "Allan", bairros: ["praia grande"] }
 ];
 
 const rotasCarro = [
-    { nome: "Falar com a torre", bairros: ["peruibe"] },
+    { nome: "Falar com a torre", bairros: ["peruibe", "praia grande"] },
     { nome: "Douglas Daniel", bairros: ["gaivota", "luiza mar", "jamaica", "jardim regina", "bopiranga", "santa julia", "sao fernando", "tupy", "cibratel"] },
     { nome: "Mauro", bairros: ["coronel", "umuarama", "sabauna", "guapiranga", "corumba", "belas artes", "praia dos sonhos", "cidade anchieta", "centro", "satelite", "vila sao paulo", "laranjeiras", "oasis", "savoy", "guapura"] },
     { nome: "Marcos", bairros: ["tropical", "nova itanhaem", "magalhaes", "sion", "suarao", "campos eliseos", "verde mar", "santa terezinha", "loty", "florida mirim", "itaguai", "agenor de campos", "regina maria", "santana", "jardim leonor", "jussara", "itaoca"] },
-    { nome: "Edmilson", bairros: ["jardim praia grande", "oceanopolis", "vila seabra", "nossa senhora de fatima", "vila atlantica", "umuarama", "vera cruz", "mongagua", "pedreira", "centro", "vila sao paulo", "vila nova", "praia grande"] }
+    { nome: "Edmilson", bairros: ["jardim praia grande", "oceanopolis", "vila seabra", "nossa senhora de fatima", "vila atlantica", "umuarama", "vera cruz", "mongagua", "pedreira", "centro", "vila sao paulo", "vila nova"] }
 ];
 
 function consultar() {
@@ -45,6 +45,7 @@ function consultar() {
     } else { boxC.style.display = 'none'; }
 
 }
+
 
 
 
